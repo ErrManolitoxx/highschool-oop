ac11f27f8606342d0a5fc7009ac4e213a0c5dc25
Allan <allanglopez1312@gmail.com>
2024-11-25 10:16:39 +0100

    En el main, invocamos el studentReader

--- src/org/ies/highschool/Main.java
+++ src/org/ies/highschool/Main.java
@@ -1,7 +1,17 @@
 package org.ies.highschool;
 
+import org.ies.highschool.components.StudentReader;
+import org.ies.highschool.model.Student;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        StudentReader studentReader = new StudentReader(scanner);
+        Student student = studentReader.read();
+
+        student.showInfo();
     }
 }
